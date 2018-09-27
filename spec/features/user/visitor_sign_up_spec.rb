b7bf8283bc36dff1f6e748b889d661833078bd66
Willian e Antônio <willianmarquesv@gmail.com>
2018-09-26 22:13:53 -0300

    Cadastro de Usuário
    Co-authored-by: Willian Vieira <willianmarquesv@gmail.com>
    Co-authored-by: Antônio Vieira <antonio.m.13@live.com>

--- spec/features/user/visitor_sign_up_spec.rb
+++ spec/features/user/visitor_sign_up_spec.rb
@@ -7,8 +7,16 @@ feature 'Visitor sign up' do
     
     fill_in 'E-mail', with: 'exemple@gmail.com'
     fill_in 'Senha', with: '12345678'
+    fill_in 'Confirme sua senha', with: '12345678'
+    fill_in 'Nome', with: 'Exemplo da Silva'
+    fill_in 'Documento', with: '12345678910'
+    fill_in 'Telefone', with: '1112345678'
     click_on 'Cadastrar'
 
-    expect(page).to have_css('Olá exemple@gmail.com')
+    expect(page).to have_content('Olá exemple@gmail.com')
+    user = User.last
+    expect(user.name).to eq("Exemplo da Silva")
+    expect(user.document).to eq("12345678910")
+    expect(user.phone).to eq("1112345678")
   end
 end
