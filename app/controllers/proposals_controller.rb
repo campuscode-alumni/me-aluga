e18889747803da2b058ea4cc6a5a042698a1498a
Willian e Antônio <willianmarquesv@gmail.com>
2018-10-04 21:06:27 -0300

    Ajuste proposals_controller
    Co-authored-by: Willian Vieira <willianmarquesv@gmail.com>
    Co-authored-by: Antônio Vieira <antonio.m.13@live.com>

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,6 +2,7 @@ class ProposalsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create]
   before_action :authenticate_realtor!, only: [:index]
   before_action :authenticate_user_or_realtor!, only: [:show]
+  before_action :set_property, only: [:create, :new, :show]
 
   def index
     @properties =  Property.where(realtor: current_realtor)
@@ -9,36 +10,33 @@ class ProposalsController < ApplicationController
   end
 
   def show
-    @property = Property.find(params[:property_id])
     @proposal = Proposal.find(params[:id])
   end
 
   def new
-    @property = Property.find(params[:property_id])
     @proposal =  Proposal.new(property: @property)
-   
   end
 
   def create
-              @property = Property.find(params[:property_id])
-              
-              @proposal = @property.proposals.new(params.require(:proposal).permit(:start_date, :end_date, :total_amount, 
-                                              :total_guests, :purpose))
-              @proposal.user = current_user                                                                
-              @proposal.set_total_amount
-          
-            if @proposal.save
-              flash[:success] = "Proposta enviada com sucesso."
-              redirect_to property_proposal_path(@property, @proposal)
-            else
-            
-              flash[:alert] = "Você deve preencher todos os campos da proposta."
-              render :new
-            end
-  
+    @proposal = @property.proposals.new(params.require(:proposal).permit(:start_date, :end_date, :total_amount, 
+                                    :total_guests, :purpose))
+    @proposal.user = current_user                                                                
+    @proposal.set_total_amount
+
+    if @proposal.save
+      flash[:success] = "Proposta enviada com sucesso."
+      redirect_to property_proposal_path(@property, @proposal)
+    else
+      flash[:alert] = "Você deve preencher todos os campos da proposta."
+      render :new
+    end
   end
 
   def authenticate_user_or_realtor!
     user_signed_in? || realtor_signed_in?
+  end
+
+  def set_property
+    @property = Property.find(params[:property_id])
   end
 end
